1e7c7adb13c43174cd9f57d458f6b05e54d1c50b
Aayush Prasad <aayushp336@gmail.com>
2024-11-26 21:02:04 +0530

    Update p105.c

--- lab 8 to 10/p105.c
+++ lab 8 to 10/p105.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+
+void swap(char* a, char* b) {
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 int main() {
 
-    char word[] = "cab";
-    int pos = 0;
+    char word[10], original[10];
+    scanf("%s", &word);
+    strcpy(original, word);
+    int start = 1;
+    
+    while (strcmp(word, original) || start) {
+        for (int j=0; j<strlen(word)-1; j++) {
+            swap(&word[j], &word[j+1]);
+            printf("%s\n", word);
+        }
+        start =0;
+    }
+    
+    
 
 
 
